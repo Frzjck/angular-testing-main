333ed648939ed9e148474fdb18a9c6d77233c428
Max Tuzenko <max.tuzenko@gmail.com>
2024-04-21 20:44:11 +0200

    test: chip test via "Test Host Strategy"

--- projects/ui-kit/src/lib/components/chip/chip.component.spec.ts
+++ projects/ui-kit/src/lib/components/chip/chip.component.spec.ts
@@ -10,7 +10,38 @@ describe('ChipComponent', () => {
       By.css('[data-testingId="chip-text"]')
     ).nativeElement;
     expect(chipTextEl.innerText).toBe('Angular Chip');
-    debugger;
+  });
+
+  it('should emit removed event when remove icon is clicked', () => {
+    let expectedValue: any;
+    const fixture = TestBed.createComponent(ChipComponent);
+    fixture.componentRef.setInput('removable', true);
+    fixture.componentInstance.removed.subscribe(
+      (chip) => (expectedValue = chip)
+    );
+    fixture.detectChanges();
+
+    const removeButton = fixture.debugElement.query(
+      By.css('[data-testingId="removeIcon"]')
+    );
+
+    removeButton.triggerEventHandler('click');
+
+    expect(expectedValue).toBe(fixture.componentInstance);
+  });
+
+  it('should emit event if remove icon is clicked (Test Host Strategy)', () => {
+    const { fixture, chipDebugEl } = setup();
+    fixture.componentInstance.removable = true;
+    fixture.detectChanges();
+    const removeIconEl = chipDebugEl.query(
+      By.css('[data-testingId="removeIcon"]')
+    );
+    removeIconEl.triggerEventHandler('click');
+
+    expect(fixture.componentInstance.removedItem).toBe(
+      chipDebugEl.componentInstance
+    );
   });
 });
 
@@ -18,9 +49,16 @@ function setup() {
   @Component({
     standalone: true,
     imports: [ChipComponent],
-    template: ` <df-chip> Angular Chip </df-chip> `,
+    template: `
+      <df-chip (removed)="removedItem = $event" [removable]="removable">
+        Angular Chip
+      </df-chip>
+    `,
   })
-  class ChipTestHostComponent {}
+  class ChipTestHostComponent {
+    removedItem!: ChipComponent<unknown>;
+    removable = false;
+  }
 
   let fixture = TestBed.createComponent(ChipTestHostComponent);
   let chipDebugEl = fixture.debugElement.query(By.directive(ChipComponent));
